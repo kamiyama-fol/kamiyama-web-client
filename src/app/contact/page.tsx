d6eaf91fc8f3f15e6792c040b88a92f05fa17cc9
KoukiFOL <kamiyama.fol@gmail.com>
2025-10-27 23:52:10 +0900

    問い合わせフォームをGoogleフォームに変更

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,48 @@
-import { socials } from '@/lib/socials';
-import Image from 'next/image';
+import { socials } from "@/lib/socials";
+import Image from "next/image";
+import FadeInSection from "@/components/FadeInSection";
 
 export default function ContactPage() {
   return (
     <main>
       <section className="text-center py-16 ">
         <h1 className="text-3xl font-bold mb-8">CONTACT</h1>
-        <p className="text-center mb-8 max-w-2xl mx-auto px-4">問い合わせは以下のメールアドレスにてお願いいたします。</p>
-        <p className='font-bold mb-8 text-2xl'>kamiyama.fol[at]gmail.com</p>
-        <p>スパムメール防止のため、@を[at]に変更しております。</p>
-        <p className="mt-8">SNSのDMでもご連絡を受け付けております。</p>
-          <div className="social-links flex justify-center space-x-6 py-4">
-          {socials.map((social) => (
-              <a key={social.name} href={social.href} target="_blank" rel="noopener noreferrer" className="text-2xl hover:text-gray-400">
-                  {social.icon ? (
-                      <social.icon />
-                  ) : (
-                      <Image src={social.image!} alt={social.alt} width={25} height={25} />
-                  )}
-              </a>
-          ))}
+        <div className="mt-8 flex justify-center px-4 w-full">
+          <div className="w-full max-w-2xl">
+            <FadeInSection className="py-0 px-0 my-8">
+              <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSfm6Y1Lzy4hoiiMomc-vFnHBi37uUircuxG42efP2y98o-vTA/viewform?embedded=true"
+                width="100%"
+                height="645"
+              >
+                読み込んでいます…
+              </iframe>
+            </FadeInSection>
           </div>
+        </div>
+        <p className="mt-8">SNSのDMでもご連絡を受け付けております。</p>
+        <div className="social-links flex justify-center space-x-6 py-4">
+          {socials.map((social) => (
+            <a
+              key={social.name}
+              href={social.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-2xl hover:text-gray-400"
+            >
+              {social.icon ? (
+                <social.icon />
+              ) : (
+                <Image
+                  src={social.image!}
+                  alt={social.alt}
+                  width={25}
+                  height={25}
+                />
+              )}
+            </a>
+          ))}
+        </div>
       </section>
     </main>
   );
